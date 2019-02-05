8c94e8f24d8d944683a87bf754a1cc576b1ffdd8
Ahnaf Hasan <ahasan1@stuy.edu>
2019-02-05 18:53:20 -0500

    merge with partner

--- 03_animations/animate.js
+++ 03_animations/animate.js
@@ -1,38 +1,56 @@
-var canvas = document.getElementById('canvas')
-var button = document.getElementById('start')
-var stop = document.getElementById('stop')
-var ctx = document.getElementById('canvas').getContext('2d')
+/*
+Team  - Mohammed Jamil, Ahnaf Hasan
+SoftDev2 pd6
+K03 - They lock us in the tower whenever we get caught ...which is often
+2019-02-04
+*/
+
+var canvas = document.getElementById('playground');
+var button = document.getElementById('circle');
+var stop = document.getElementById('stop');
+/** @type {CanvasRenderingContext2D} */ // allows intellisense of CanvasElements
+var ctx = canvas.getContext('2d');
 
 var requestID;
-var radius;
+var radius = 0;
 var growing = true;
 
 function clear() {
-    if (canvas.clientWidth / 2 == radius) {
-        growing = false
-    }
-    if (radius <= 0) {
-        growing = true
-    }
-    ctx.clearRect(0, 0, canvas.clientWidth, canvas.clientHeight)
-    if (growing) {
-        radius += 1
-    }else{
-        radius -= 1
-    }
-    return true
+
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+    return true;
 }
 
 function animate() {
-    clear()
-    make_circle()
+    window.cancelAnimationFrame(requestID);
+    clear();
+    make_circle();
+    requestID = window.requestAnimationFrame(animate);
 }
+
+function stopIt() {
+    window.cancelAnimationFrame(requestID);
+}
+
 
 function make_circle() {
-    ctx.beginPath()
-    ctx.arc(canvas.width / 2, canvas.height / 2, radius, 0, 2 * Math.PI)
-    ctx.stroke()
-    ctx.fill()
+    if (canvas.width / 2 == radius) {
+        growing = false;
+    }
+    if (radius <= 0) {
+        growing = true;
+    }
+    ctx.beginPath();
+    ctx.arc(canvas.width / 2, canvas.height / 2, radius, 0, 2 * Math.PI);
+    ctx.stroke();
+    ctx.fillStyle = "#00ffff";
+    ctx.fill();
+    if (growing) {
+        radius += 5;
+    } else {
+        radius -= 5;
+    }
 }
-
-button.addEventListener('click', window.requestAnimationFrame(animate))
+button.addEventListener('click', animate);
+stop.addEventListener('click', stopIt);
