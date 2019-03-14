e147a62a7221b2beada53046ad8dd6ed7c889ad2
Ahnaf Hasan <ahasan1@stuy.edu>
2019-03-13 21:46:40 -0400

    add headers. fix some code

--- 10_svg/script.js
+++ 10_svg/script.js
@@ -1,7 +1,18 @@
+/**
+ * Ahnaf Hasan
+ * SoftDev2 pd06
+ * K10 -- Ask Circles [Change || Die]
+ * 2019-03-13
+ */
+
 var svg = document.getElementById('svg')
 var cls = document.getElementById('clear')
 var mv = document.getElementById('move')
 var requestID, moving = false
+/**
+ * Adds a dot to the SVG image. Dots can change color
+ * @param {MouseEvent} e a primary mouse click
+ */
 var add_dot = function(e) {
     var c = document.createElementNS("http://www.w3.org/2000/svg", "circle")
     c.addEventListener('click', change_dot )
@@ -15,7 +26,7 @@ var add_dot = function(e) {
 }
 
 /**
- * Changes the color of the dot that was clicked on
+ * Changes the color of the dot that was clicked on. Can also change location
  * @param {MouseEvent} e a mouse primary click
  */
 var change_dot = function(e) {
@@ -30,16 +41,22 @@ var change_dot = function(e) {
         e.target.setAttribute('cy', y )
     }
 }
-
+/**
+ * Moves dots across the SVG image.
+ * Does not work. Dots glide along the edges.
+ */
 var move_dots = function() {
     window.cancelAnimationFrame(requestID)
     var w = svg.getAttribute('width')
     var h = svg.getAttribute('height')
     var elems = svg.children
+    var vels = new Array(elems.length)
+    vels.fill([1, 1])
+    console.log(vels)
     console.log(elems)
     for (i = 0; i < elems.length; i++) {
-        var velX = 1
-        var velY = 1
+        var velX = vels[i][0]
+        var velY = vels[i][1]
         var currElem = elems.item(i)
         console.log(currElem)
         var x = Number(currElem.getAttribute('cx'))
@@ -48,19 +65,24 @@ var move_dots = function() {
         var radius = Number(currElem.getAttribute('r'))
         console.log(`this is w & h: ${w}  ${h}`)
         if (x + radius + velX >= w || x + radius + velX <= 0) {
-            velX = -velX
+            vels[i][0] = -velX
+            console.log("vel changed x")
         }
         if (y + radius + velX >= h || y + radius + velY <= 0) {
-            velY = -velY
+            vels[i][1] = -velY
+            console.log(`vel changed y`)
         }
-        currElem.setAttribute('cx', `${x + velX}`)
-        currElem.setAttribute('cy', `${y + velY}`)
+        currElem.setAttribute('cx', `${x + vels[i][0]}`)
+        currElem.setAttribute('cy', `${y + vels[i][1]}`)
         currElem.setAttribute('fill', color)
         svg.replaceChild(currElem, elems.item(i))
     }
     requestID = window.requestAnimationFrame(move_dots)
 }
 
+/**
+ * Clears the SVG image
+ */
 var clear = function() {
     while (svg.hasChildNodes()) {
         svg.removeChild(svg.lastChild)
