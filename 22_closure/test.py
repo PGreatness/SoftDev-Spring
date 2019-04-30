4e6927233ebc6b3b48946a8a44ab3247dd1d99b0
Ahnaf Hasan <ahasan1@stuy.edu>
2019-04-30 19:43:26 -0400

    finish homework 22

--- 22_closure/test.py
+++ 22_closure/test.py
@@ -24,20 +24,7 @@ contains_15 = outer(15)
 print(contains_15([1, 4, 6, 14, 15, 17, 18, 19]))
 
 
-def repeat(word):
-    def num(number):
-        a = ''
-        for x in range(number):
-            a += word
-        return a
-    return num
 
-r1 = repeat('hello')
-r2 = repeat('goodbye')
-
-print(r1(2))
-print(r2(2))
-print(repeat('cool')(3))
 
 def outer():
     x = 'foo'
@@ -55,13 +42,37 @@ def make_counter():
         nonlocal count
         count += 1
         return count
-    return inc
+    def access():
+        nonlocal count
+        return count
+    return inc, access
 
-c1 = make_counter()
-c2 = make_counter()
+c1, curr1 = make_counter()
+c2, curr2 = make_counter()
 
 print(c1())
+print(f"Current 1: {curr1()}")
 print(c1())
+print(f"Current 1: {curr1()}")
 print(c2())
+print(f"Current 2: {curr2()}")
 print(c1())
+print(f"Current 1: {curr1()}")
 print(c2())
+print(f"Current 2: {curr2()}")
+print(f"Current 1: {curr1()}")
+
+def repeat(word):
+    def num(number):
+        a = ''
+        for x in range(number):
+            a += word
+        return a
+    return num
+
+r1 = repeat('hello')
+r2 = repeat('goodbye')
+
+print(r1(2))
+print(r2(2))
+print(repeat('cool')(3))
